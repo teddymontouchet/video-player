0d434db350bd34764bdabbbf393af963e61f18ef
Teddy Montouchet <teddy.montouchet@outlook.fr>
2023-08-05 15:21:05 +0200

    first commit

---
+++ src/main/java/com/openclassrooms/testing/videoplayer/App.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.testing.videoplayer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
